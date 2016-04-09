04dd36bd8cb1bc8af7b099bcd655ece3f4015af2
Andrés Peña <lapc506@users.noreply.github.com>
2016-04-09 13:00:37 -0600

    Update and rename ConnectionFactory to ConnectionFactory.java

--- src/org/parquelibertad/controller/ConnectionFactory.java
+++ src/org/parquelibertad/controller/ConnectionFactory.java
@@ -11,8 +11,11 @@ public class ConnectionFactory {
         	DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
             return DriverManager
               .getConnection(
-                "jdbc:oracle:thin:" + localhost + "/" +
-                  portNumber.toString(), schemaUsername, schemaPassword);
+                "jdbc:oracle:thin:" + localhost + "/" + portNumber.toString(),
+                // Using THIN driver version as suggested in:
+                // http://stackoverflow.com/questions/21711085/what-is-the-differente-between-oci-and-thin-driver-connection-with-data-source-c
+                // based on https://en.wikipedia.org/wiki/JDBC_driver Type-4 driver
+                  schemaUsername, schemaPassword);
         }
 
 }
