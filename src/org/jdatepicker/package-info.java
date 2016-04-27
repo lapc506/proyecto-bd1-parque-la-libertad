c40e9dd7e13f62eed62a82bf8ed373145084178d
Andrés Peña Castillo (XPVM) <lapc20081996@gmail.com>
2016-04-27 17:22:38 -0600

    Importación de la biblioteca JDatePicker
    Copyright 2004 Juan Heyns. All rights reserved.
    https://github.com/JDatePicker/JDatePicker/blob/master/LICENSE.md

---
+++ src/org/jdatepicker/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * proyecto-bd1-parque-la-libertad
+ * org.jdatepicker
+ * @author Juan Heyns https://github.com/JDatePicker/JDatePicker 
+ * Refer to Simplidied BSD LICENSE.md
+ *
+ */
+package org.jdatepicker;
